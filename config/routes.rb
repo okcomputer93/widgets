5e10d5709b1e3f9b10a22adce0e0a8f6c7739148
Omar Juárez <omar@easybroker.com>
2022-05-28 21:34:00 -0500

    Always use Canonical Routes that Conform to Rails Defaults

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :widgets, only: [:show, :index]
+  get 'manufacturer/:id', to: 'manufacturers#show'
+
+  ###
+  # Custom routes start her
+  #
+  # For each new custom route:
+  #
+  # * Be sure you have the canonical route declared above
+  # * Add the new custom route below the existing ones
+  # * Document why it's needed
+  # * Explain anything else non-standar
+
+  # Used in podcast ads for the 'amazing' campaign
+  get '/amazing', to: redirect('/widgets')
+  # or
+  # get '/amazing', to: 'widgets'
+
 end
