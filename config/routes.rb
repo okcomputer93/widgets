cd3762d9726e83b403426166603a658fcaa0e1b3
Omar Juárez <omar@easybroker.com>
2022-05-28 22:48:25 -0500

    Rails works best when you are resource-focused, not action-focused

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :widgets, only: [:show, :index]
+  resources :widget_ratings, only: [:create]
+
+  # Don't
   get 'manufacturer/:id', to: 'manufacturers#show'
 
   ###
